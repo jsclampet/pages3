7b53c80b70d1f5338b7ff3ad3ec04cb79a40ea1a
john clampet <jsclampet@gmail.com>
2023-05-27 18:58:06 -0500

    custom directive || replacing "ngClass" with a customer class the replicates it

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -8,9 +8,16 @@ export class ClassDirective {
   constructor(private element: ElementRef) { 
 
    }
-
-   @Input('appClass') set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+   // using "any" type for Object
+   @Input('appClass') set classNames(classObj: any) {
+    // for-in loop to iterate through object
+    for (let key in classObj) {
+      if (classObj[key]){
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
    }
 
 }
